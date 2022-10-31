eb2b8c5189605c95b63ceaef3200befe4982ca9d
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-10-31 19:27:29 -0300

    Criando a Views.

---
+++ resources/views/compositores/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('title', 'Listagem de Compositores')
+@section('content')
+<h1>Listagem de Compositores</h1>
+<ul>
+    @foreach($compositores as $compositor)
+    <li>
+        <a href="{{url('compositores/'.$compositor->id)}}">
+            {{$compositor->nome}}</a>
+    </li>
+    @endeforeach
+</ul>
+@endesection
