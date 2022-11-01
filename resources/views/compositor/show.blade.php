a46a4f2f5b00d672ee95e10236c56e566419ee41
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-01 17:44:54 -0300

    Funcionando, observar sempre a sintaxe e variáreis.

--- resources/views/compositor/show.blade.php
+++ resources/views/compositor/show.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <h1>Compositor - {{$compositor->nome}}</h1>
 <ul>
-    <li>ID: {{$contato->id}}</li>
+    <li>ID: {{$compositor->id}}</li>
     <li>Nome: {{$compositor->nome}}</li>
     <li>Ano: {{$compositor->ano}}</li>
     <li>Origem: {{$compositor->origem}}</li>
     <li>Resumo: {{$compositor->resumo}}</li>
     <li>Obras: {{$compositor->obras}}</li>
     </ul>
-    <a href="{{url('compositor')}}">voltar</a>
+    <a href="{{url('compositores')}}">voltar</a>
 @endsection
