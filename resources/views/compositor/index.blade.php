9ec2c4b4e737f5691bf023900dc7af26c822284e
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-07 18:09:09 -0300

    Trocado o< li> pelo <table> e incluido o button create.

--- resources/views/compositor/index.blade.php
+++ resources/views/compositor/index.blade.php
@@ -2,15 +2,25 @@
 @section('title','Listagem de Compositores')
 @section('content')
 <h1>Listagem de Compositores</h1>
+@if(Session::has('mensagem'))
+<div class="alert alert-info">
+    {{Session::get('mensagem')}}
+</div>
+@endif
 
-<ul>
+<br />
+<a class="btn btn-success" href="{{url('compositores/create')}}">Criar</a>
+<br /><br />
+<table class="table table-striped">
     @foreach ($compositores as $compositor)
-    <li>
-        <a href="{{url('compositores/'.$compositor->id)}}">
-        {{$compositor->nome}}</a>
-    </li>
+    <tr>
+        <td>
+            <a href="{{url('compositores/'.$compositor->id)}}">
+                {{$compositor->nome}}</a>
+        </td> 
+    </tr>
     @endforeach
-    </ul>
+</table>
 @endsection
      
            
