5d220b626f7752f5432014ecdd21a48e36ed3f9e
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-07 21:21:53 -0300

    Criada a pasta para inserir novos compositores.

---
+++ resources/views/compositor/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+@section('title','Inserir Compositor')
+@section('content')
+<h1>Inserir Compositor</h1>
+<br />
+{{Form::open(['route' => 'compositores.store', 'method' => 'POST'])}}
+{{Form::label('nome', 'Nome')}}
+{{Form::text('nome','',['class'=>'form-control','required',
+'placeholder'=>'Nome do Compositor'])}}
+{{Form::label('ano', 'Ano')}}
+{{Form::text('ano','',['class'=>'form-control','required',
+'placeholder'=>'Ano de atuação'])}}
+{{Form::label('origem', 'Origem')}}
+{{Form::text('origem','',['class'=>'form-control','required',
+'placeholder'=>'Origem do Compositor'])}}
+{{Form::label('resumo', 'Resumo')}}
+{{Form::text('resumo','',['class'=>'form-control','required',
+'placeholder'=>'Insira um resumo'])}}
+{{Form::label('obras', 'Obras')}}
+{{Form::text('obras','',['class'=>'form-control','required',
+'placeholder'=>'Obras do Compositor'])}}
+<br />
+{{Form::submit('Salvar',['class'=>'btn btn-success'])}}
+{!!Form::button('Cancelar',['onclick'=>'javascript:history.go(-1)', 
+'class'=>'btn btn-secundary'])!!}
+{{Form::close()}}
+@endsection
