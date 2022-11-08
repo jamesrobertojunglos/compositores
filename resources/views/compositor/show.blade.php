7def8384b314c12a86c2eabbd93440bebab666ae
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-08 14:28:04 -0300

    Foi incluido as Divs, evento onclick para comfirmar o botão excluir.

--- resources/views/compositor/show.blade.php
+++ resources/views/compositor/show.blade.php
@@ -1,19 +1,25 @@
 @extends('layout.app')
 @section('title','Compositor - {{$compositor->nome}}')
 @section('content')
-<h1>Compositor - {{$compositor->nome}}</h1>
-<ul>
-    <li>ID: {{$compositor->id}}</li>
-    <li>Nome: {{$compositor->nome}}</li>
-    <li>Ano: {{$compositor->ano}}</li>
-    <li>Origem: {{$compositor->origem}}</li>
-    <li>Resumo: {{$compositor->resumo}}</li>
-    <li>Obras: {{$compositor->obras}}</li>
-    </ul>
-
+<div class="card">
+    <div class="card-header">
+    <h1>Compositor - {{$compositor->nome}}</h1>
+    </div>
+    <div class="card-body">
+        <h3 class="card-title">ID: {{$compositor->id}}</h3>
+        <p class="text"><br/>
+        Nome: {{$compositor->nome}}<br/>
+        Ano: {{$compositor->ano}}<br/>
+        Origem: {{$compositor->origem}}<br/>
+        Resumo: {{$compositor->resumo}}<br/>
+        Obras: {{$compositor->obras}}<br/>
+</div>
+<div class="card-footer">
     {{Form::open(['route'=> ['compositores.destroy',$compositor->id],'method' => 'DELETE'])}}
     <a href="{{url('compositores/'.$compositor->id.'/edit')}}" class="btn btn-success">Alterar</a>
-    {{Form::submit('Excluir',['class'=>'btn btn-danger'])}}
+    {{Form::submit('Excluir',['class'=>'btn btn-danger','onclick'=>'return confirm("Confima a exclusão?")'])}}
     <a href="{{url('compositores/')}}" class="btn btn-secondary">Voltar</a>
     {{Form::close()}}
+</div>
+</div>
 @endsection
