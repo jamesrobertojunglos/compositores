357bde0a3c45e682f2258333e46b2447a1da202a
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-08 11:09:22 -0300

    Formulário para edição criado.

--- resources/views/compositor/edit.blade.php
+++ resources/views/compositor/edit.blade.php
@@ -2,17 +2,6 @@
 @section('title','Alteração Compositor {{$compositor->nome}}')
 @section('content')
 <h1>Alteração Compositor {{$compositor->nome}}</h1>
-@if(count($errors) > 0)
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>
-            {{error}}
-        </li>
-        @endforeach
-    </ul>
-</div>
-@endif<br />
 @if(Session::has('mensagem'))
 <div class="alert alert-success">
     {{Session::get('mensagem')}}
@@ -20,23 +9,23 @@
 @endif
 <br \>
 {{Form::open(['route' => ['compositores.update',$compositor->id], 'method' => 'PUT'])}}
-{{Form::label('nome', 'Nome')}}
-{{Form::text('nome',$compositor->nome,['class'=>'form-control'
-,'required', 'placeholder'=>'Nome do Compositor'])}}
-{{Form::label('ano', 'Ano')}}
-{{Form::text('ano',$compositor->ano,['class'=>'form-control'
-,'required', 'placeholder'=>'Periodo em que o compositor atuou'])}}
-{{Form::label('origem', 'Origem')}}
-{{Form::text('origem',$compositor->origem,['class'=>'form-control'
-,'required', 'placeholder'=>'Insira a origem do Compositor'])}}
-{{Form::label('resumo', 'Resumo')}}
-{{Form::text('resumo',$compositor->resumo,['class'=>'form-control'
-,'required', 'placeholder'=>'Insira um resumo sobre o Compositor'])}}
-{{Form::label('obras', 'Obras')}}
-{{Form::text('obras',$compositor->obras,['class'=>'form-control'
-,'required', 'placeholder'=>'insira as obras do Compositor'])}}
-<br />
-{{Form::submit('Salvar',['class'=>'btn btn-success'])}}
-<a href="{{url('compositores')}}" class="btn btn-secondary">Voltar</a>
-{{Form::close()}}
-@endsection
+    {{Form::label('nome', 'Nome')}}
+    {{Form::text('nome',$compositor->nome,['class'=>'form-control'
+    ,'required', 'placeholder'=>'Nome do Compositor'])}}
+    {{Form::label('ano', 'Ano')}}
+    {{Form::text('ano',$compositor->ano,['class'=>'form-control'
+    ,'required', 'placeholder'=>'Periodo em que o compositor atuou'])}}
+    {{Form::label('origem', 'Origem')}}
+    {{Form::text('origem',$compositor->origem,['class'=>'form-control'
+    ,'required', 'placeholder'=>'Insira a origem do Compositor'])}}
+    {{Form::label('resumo', 'Resumo')}}
+    {{Form::text('resumo',$compositor->resumo,['class'=>'form-control'
+    ,'required', 'placeholder'=>'Insira um resumo sobre o Compositor'])}}
+    {{Form::label('obras', 'Obras')}}
+    {{Form::text('obras',$compositor->obras,['class'=>'form-control'
+    ,'required', 'placeholder'=>'insira as obras do Compositor'])}}
+    <br />
+    {{Form::submit('Salvar',['class'=>'btn btn-success'])}}
+    <a href="{{url('compositores')}}" class="btn btn-secondary">Voltar</a>
+    {{Form::close()}}
+    @endsection
