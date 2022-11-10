9d90f3940b0812a4d0eda4138bfd02fe71cad795
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-09 22:19:44 -0300

    Menu do layout criado.

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-sm bg-blue navbar-dark">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="{{url('/')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('compositores/')}}">Compositores Clássicos</a>
+                </li>
+                <li class='nav-item'>
+                    <a class="nav-link" href="{{url('obras/')}}">Partituras completas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
