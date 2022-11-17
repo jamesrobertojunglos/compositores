c63a8a4f8ccf54fd8e0cade55479d7ae2386643b
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-16 23:50:21 -0300

    Criando a Migration e executando.

--- database/migrations/2022_11_16_233626_create_partituras_table.php
+++ database/migrations/2022_11_16_233626_create_partituras_table.php
@@ -15,6 +15,8 @@ class CreatePartiturasTable extends Migration
     {
         Schema::create('partituras', function (Blueprint $table) {
             $table->id();
+            $table->string('compositor',200);
+            $table->string('partituras',20000);
             $table->timestamps();
         });
     }
