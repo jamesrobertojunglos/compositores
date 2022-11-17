5d2da9e819f8f86d25583d8c496a81bbd226a3a5
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-11-16 23:40:56 -0300

    Criada a Migration Partitura.

---
+++ app/Http/Controllers/PartiturasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Partituras;
+use Illuminate\Http\Request;
+
+class PartiturasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Partituras  $partituras
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Partituras $partituras)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Partituras  $partituras
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Partituras $partituras)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Partituras  $partituras
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Partituras $partituras)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Partituras  $partituras
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Partituras $partituras)
+    {
+        //
+    }
+}
